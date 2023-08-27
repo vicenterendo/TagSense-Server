891717dcfeb0fdd5adce31d7df1d14b6429779e2
Vicente Rendo <vicente.rendo@gmail.com>
2023-08-27 09:35:59 +0100

    feat(dependencies): add get_db dependency

--- src/routers/v0/flight.py
+++ src/routers/v0/flight.py
@@ -1,36 +1,28 @@
-from fastapi import APIRouter
-from fastapi import FastAPI, Response, Request
-from typing import List
-from pydantic import BaseModel
-from ...database import SessionLocal
-from ... import models, schemas, crud, utils
+from fastapi import APIRouter, Depends
+from fastapi import Response, Request
+from ... import schemas, crud, dependencies
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 @router.post("/flight")
-def post_tag(res: Response, req: Request, body: list[schemas.FlightCreate]):
+def post_tag(res: Response, req: Request, body: list[schemas.FlightCreate], db: Session = Depends(dependencies.get_db)):
   flight: list[schemas.Flight] = []
   for flight_schema in body:
-    session = SessionLocal()
-    db_flight = crud.set_flight(session, flight_schema)
+    db_flight = crud.set_flight(db, flight_schema)
     if not db_flight: continue
     dict_flight = db_flight.__dict__
     dict_flight.pop("_sa_instance_state")
     if not dict_flight: continue
     flight.append(schemas.Flight(**dict_flight))
-    session.close()
   return flight
 
 @router.get("/flight/{callsign}", response_model=list[schemas.FlightGet])
-def get_tag(callsign: str, res: Response, req: Request):
-  session = SessionLocal()
-  flights = crud.get_flights(session, valid_only=True, callsign=callsign)
-  session.close()
+def get_tag(callsign: str, res: Response, req: Request, db: Session = Depends(dependencies.get_db)):
+  flights = crud.get_flights(db, valid_only=True, callsign=callsign)
   return flights
 
 @router.get("/flight", response_model=list[schemas.FlightGet])
-def get_all_tags(res: Response, req: Request):
-  session = SessionLocal()
-  flights = crud.get_flights(session, valid_only=True)
-  session.close()
+def get_all_tags(res: Response, req: Request, db: Session = Depends(dependencies.get_db)):
+  flights = crud.get_flights(db, valid_only=True)
   return flights
